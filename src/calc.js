ad6a8950b12c16a7d1ab906e392e4744dc9d6eb4
fulsep <57238803+fulsep@users.noreply.github.com>
2026-07-02 09:40:59 +0700

    fix: handle non-number parameter values with throws error message

--- src/calc.js
+++ src/calc.js
@@ -1,3 +1,6 @@
 export function sum(num1, num2){
+    if(typeof num1 !== "number" || typeof num2 !== "number"){
+        throw new Error("parameter(s) should be a number")
+    }
     return num1 + num2
 }
